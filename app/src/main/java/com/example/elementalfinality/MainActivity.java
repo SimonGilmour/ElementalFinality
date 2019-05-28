75623690e7c4a50b6dda152e44252fa602d05e08
FalsettoReverie <Simonrgilmour@gmail.com>
2019-05-27 22:15:23 -0600

    "added element images"

--- app/src/main/java/com/example/elementalfinality/MainActivity.java
+++ app/src/main/java/com/example/elementalfinality/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.elementalfinality;
 
-import android.animation.Animator;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
